c9a7ab737b6ac40c535274cfade1ca0c3539214e
skynex-xd <56487521+skynex-xd@users.noreply.github.com>
2024-11-28 18:13:25 +0300

    Revert "HW3_DONE"

--- tasks/local_shadertoy1/App.hpp
+++ tasks/local_shadertoy1/App.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
-#include <etna/GlobalContext.hpp>
 #include <etna/Window.hpp>
 #include <etna/PerFrameCmdMgr.hpp>
 #include <etna/ComputePipeline.hpp>
 #include <etna/Image.hpp>
-#include <etna/BlockingTransferHelper.hpp>
-#include <etna/Sampler.hpp>
 
 #include "wsi/OsWindowingManager.hpp"
 
@@ -31,19 +28,4 @@ private:
 
   std::unique_ptr<etna::Window> vkWindow;
   std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
-
-  etna::ComputePipeline pipeline;
-  etna::Image image;
-  etna::Sampler sampler;
-  glm::vec2 mouse_pos;
-  std::chrono::time_point<std::chrono::system_clock> init_time = std::chrono::system_clock::now();
-
-  struct Params
-  {
-    glm::vec2 resolution;
-    glm::vec2 mouse_pos;
-    float time;
-  };
-
-  Params params;
 };
